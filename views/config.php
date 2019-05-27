462e4fb78eb02f2ab0237f3af29c37a51eb3dcb3
Luca Labate <labatel@protonmail.ch>
2019-05-27 14:14:33 +0200

    test

--- views/config.php
+++ views/config.php
@@ -28,20 +28,3 @@ if(isset($_POST['submit'])) {
     }
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-		<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-    <title><?php echo $this->aContent['pageTitle']; ?></title>
-    <?php
-    //CHARGE TOUTES LES PAGES CSS
-    foreach($this->aContent['css'] as $sCssPath) {
-      echo "<link rel='stylesheet' href='".$sCssPath."'>";
-    }
-    //CHARGE TOUTES LES PAGES JS
-    foreach($this->aContent['js'] as $sJsPath){
-      echo "<script src='".$sJsPath."'></script>";
-    }
-     ?>
