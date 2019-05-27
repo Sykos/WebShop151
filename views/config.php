e77d7742e5ff6a2f5258c1a8371702c6ba14a996
Luca Labate <labatel@protonmail.ch>
2019-05-27 15:01:24 +0200

    Update config.php

--- views/config.php
+++ views/config.php
@@ -1,12 +1,14 @@
 <?php
 // Include config file
-
-$hostname = "127.0.0.1:49386";
-$username = "azure";
-$password = "X6#vWHD_$";
-$db = "raclettev2";
-
-$dbconnect=mysqli_connect($hostname,$username,$pass,$db);
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', '127.0.0.1:49386');
+define('DB_USERNAME', 'azure');
+define('DB_PASSWORD', 'x6#vWHD_$');
+define('DB_NAME', 'raclettev2');
+ 
+/* Attempt to connect to MySQL database */
+$dbconnect = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 if ($dbconnect->connect_error) {
   die("Database connection failed: " . $dbconnect->connect_error);
