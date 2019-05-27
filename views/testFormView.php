1c42ea4d6359f75eb380e7a6cdecb7e15440f91e
Luca Labate <labatel@protonmail.ch>
2019-05-27 13:41:12 +0200

    Update testFormView.php

--- views/testFormView.php
+++ views/testFormView.php
@@ -1,17 +1,8 @@
-	<?php/*
-	require_once "config.php";
-	$sql = "INSERT INTO `utilisateurs`(`UtilisateursNomDeFamille`) VALUES (".nom")";
-	if ($link->query($sql) === TRUE) {
-    echo "New record created successfully";
-		} else {
-    echo "Error: " . $sql . "<br>" . $link->error;
-}*/
-     ?>
 <?php
 // Include config file
 require_once "config.php";
 
-$toto = "INSERT INTO `utilisateurs`(`UtilisateursPrenom`) VALUES ('toto')";
+$toto = "INSERT INTO `utilisateurs`(`UtilisateursNomDeFamille`) VALUES ('$nom')";
 
 if ($link->query($toto) === TRUE) {
     echo "New record created successfully";
