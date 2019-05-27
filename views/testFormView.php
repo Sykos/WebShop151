94e7b4e39040137541851ebc9bf1b45b841f5185
Luca Labate <labatel@protonmail.ch>
2019-05-27 12:03:42 +0200

    Je fais votre taff

--- views/testFormView.php
+++ views/testFormView.php
@@ -2,6 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+		 <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <title><?php echo $this->aContent['pageTitle']; ?></title>
     <?php
     //CHARGE TOUTES LES PAGES CSS
