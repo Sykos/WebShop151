b22d442f58a2b5876bfb5a4a194bb7f7a64d6987
Luca Labate <labatel@protonmail.ch>
2019-05-27 14:06:48 +0200

    Update testFormView.php

--- views/testFormView.php
+++ views/testFormView.php
@@ -1,6 +1,16 @@
 <?php
 // Include config file
-require_once "config.php";
+
+$hostname = "127.0.0.1:49386";
+$username = "azure";
+$password = "6#vWHD_$";
+$db = "raclettev2";
+
+$dbconnect=mysqli_connect($hostname,$username,$password,$db);
+
+if ($dbconnect->connect_error) {
+  die("Database connection failed: " . $dbconnect->connect_error);
+}
 
 if(isset($_POST['submit'])) {
   $nom=$_POST['name'];
@@ -10,7 +20,13 @@ if(isset($_POST['submit'])) {
 
     $query = "INSERT INTO utilisateurs (UtilisateursNomDeFamille, UtilisateursPrenom, UtilisateursEmailVerifiee, UtilisateursMotDePasse)
     VALUES ('$nom', '$prenom', '$mail', '$pass')";
-  
+	
+	   if (!mysqli_query($dbconnect, $query)) {
+        die('An error occurred when submitting your review.');
+    } else {
+      echo "Thanks for your review.";
+    }
+}
 ?>
 
 <!DOCTYPE html>
