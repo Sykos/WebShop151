dcfa228fad46575f712e7093d68c2f0143eae939
Sykos <labatel@protonmail.ch>
2019-06-08 13:27:23 +0200

    Update signInController.php

--- controllers/signInController.php
+++ controllers/signInController.php
@@ -49,12 +49,12 @@
 // Include config file
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', '127.0.0.1:49386');
+/*define('DB_SERVER', '127.0.0.1:49386');
 define('DB_USERNAME', 'azure');
 define('DB_PASSWORD', '6#vWHD_$');
 define('DB_NAME', 'raclettev2');
  
-/* Attempt to connect to MySQL database */
+/* Attempt to connect to MySQL database *//*
 $dbconnect = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 if (mysqli_connect_errno()) {
@@ -76,5 +76,5 @@ if ($dbconnect->query($query) === TRUE) {
 } else {
 	die($dbconnect->error);
 }
-}
+}*/
 ?>
