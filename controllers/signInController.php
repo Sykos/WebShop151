3f98f0e8063a08fc8d55b050c62410b620bd4bb7
Sykos <labatel@protonmail.ch>
2019-06-08 13:33:22 +0200

    Update signInController.php

--- controllers/signInController.php
+++ controllers/signInController.php
@@ -2,17 +2,17 @@
 
   class Controller extends mainController {
 
-    //SE LANCE AUTOMATIQUEMENT ET CHARGE TOUT LE CONTENU
-    public function __construct() {
+    public function __construct(){
 
       parent::__construct();
 
-      $this->aContent['viewPath'] = '../views/signInView.php';
+      $this->aContent['viewPath'] = './views/signInView.php';
       $this->aContent['pageTitle'] = 'Inscription';
-      
+    }
 
-    }
     public function render(){
-        parent::render();
+      parent::render();
     }
+
   }
+
