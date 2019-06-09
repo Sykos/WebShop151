13125d7f8260f104e7e2ef3706e362e06bc4be88
Lem0n33 <thedeadteam1814@gmail.com>
2019-06-09 10:11:34 +0200

    Ajout affichage message d'erreur

--- controllers/signInController.php
+++ controllers/signInController.php
@@ -63,6 +63,10 @@ function checkData(){
   return $bCheckOk;
 }
 }
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if(isset($_POST['submit'])){
   checkData();
   echo 'Test Valide';
