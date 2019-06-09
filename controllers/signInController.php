fba0fced46485288215e83fd58e3dc7519f64b8e
Lem0n33 <thedeadteam1814@gmail.com>
2019-06-09 14:46:05 +0200

    Test register

--- controllers/signInController.php
+++ controllers/signInController.php
@@ -18,41 +18,41 @@
       parent::render();
 
     }
-   function db()
-   {
-    // Include config file
-      define('DB_SERVER', '127.0.0.1:49386');
-      define('DB_USERNAME', 'azure');
-      define('DB_PASSWORD', '6#vWHD_$');
-      define('DB_NAME', 'raclettev2');
+    public static function db()
+    {
+      // Include config file
+        define('DB_SERVER', '127.0.0.1:49386');
+        define('DB_USERNAME', 'azure');
+        define('DB_PASSWORD', '6#vWHD_$');
+        define('DB_NAME', 'raclettev2');
 
-      // Attempt to connect to MySQL database
-      $dbconnect = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+        // Attempt to connect to MySQL database
+        $dbconnect = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-      if (mysqli_connect_errno())
-      {
-        die("Database connection failed: " . mysqli_connect_error());
-      }
-
-      if(isset($_POST['submit']))
-      {
-        // checkData();
-        $nom=$_POST['nom'];
-        $prenom=$_POST['prenom'];
-        $email=$_POST['email'];
-        $password=$_POST['password'];
-        $user=$_POST['username'];
-
-        $query = "INSERT INTO `utilisateurs` (`UtilisateursNomDeFamille`, `UtilisateursPrenom`, `UtilisateursEmail`, `UtilisateursMotDePasse`,`UtilisateursUser`) VALUES ('$nom', '$prenom', '$email', '$password','$user')";
-
-        if ($dbconnect->query($query) === TRUE)
+        if (mysqli_connect_errno())
         {
-            echo "New record created successfully";
-        } else
-        {
-        	die($dbconnect->error);
+          die("Database connection failed: " . mysqli_connect_error());
         }
-      }
+
+        if(isset($_POST['submit']))
+        {
+          // checkData();
+          $nom=$_POST['nom'];
+          $prenom=$_POST['prenom'];
+          $email=$_POST['email'];
+          $password=$_POST['password'];
+          $user=$_POST['username'];
+
+          $query = "INSERT INTO `utilisateurs` (`UtilisateursNomDeFamille`, `UtilisateursPrenom`, `UtilisateursEmail`, `UtilisateursMotDePasse`,`UtilisateursUser`) VALUES ('$nom', '$prenom', '$email', '$password','$user')";
+
+          if ($dbconnect->query($query) === TRUE)
+          {
+              echo "New record created successfully";
+          } else
+          {
+          	die($dbconnect->error);
+          }
+        }
     }
     public static function checkData()
     {
@@ -80,6 +80,9 @@
 # TRAITEMENT DES DONNÉES   #
 #                          #
 ############################
+$sErrorMessage = '';
+
+//CHECK DES DONNÉES ET ENREGISTREMENT
 if(isset($_POST['submit']))
 {
   $bError = Controller::checkData();
@@ -88,7 +91,12 @@ if(isset($_POST['submit']))
     $sErrorMessage = 'Données invalides';
   }elseif ($bError == true)
   {
-    $sErrorMessage = 'Données valides';
+    Controller::db();
+    echo 'Enregistrement ok'
   }
-  echo $sErrorMessage;
+  if ($sErrorMessage != '')
+  {
+    echo $sErrorMessage;
+  }
+
 }
