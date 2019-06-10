e836b87e58c2f1cefe36aa8444e0a7955bfda7b6
Lem0n33 <thedeadteam1814@gmail.com>
2019-06-10 22:24:21 +0200

    debug login

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -31,13 +31,14 @@
 
     if($_SERVER["REQUEST_METHOD"] == "POST")
     {
-        echo 'Test validé';
-        /*// username and password sent from form
+
+        // username and password sent from form
 
         $myusername = mysqli_real_escape_string($db,$_POST['UtilisateursNomDeFamille']);
         $mypassword = mysqli_real_escape_string($db,$_POST['UtilisateursMotDePasse']);
 
-        $sql = "SELECT id FROM utilisateurs WHERE UtilisateursNomDeFamille = '$myusername' and UtilisateursMotDePasse = '$mypassword'";
+        echo 'Test validé';
+        /*$sql = "SELECT id FROM utilisateurs WHERE UtilisateursNomDeFamille = '$myusername' and UtilisateursMotDePasse = '$mypassword'";
         $result = mysqli_query($db,$sql);
         $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
         $active = $row['active'];
