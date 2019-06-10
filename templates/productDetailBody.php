ecb75a3661a16101ec302c0350018197f0ea3203
Damsodo <damienuzzz@gmail.com>
2019-06-10 21:16:53 +0200

    sa

--- templates/productDetailBody.php
+++ templates/productDetailBody.php
@@ -2,7 +2,8 @@
     <img src="" alt="product image">
 </div>
 <div class="productDescription">
-    <h1>Ici se trouvera la description des produits</h1>
+  <a href="?page=productDetail"><img src='../public/images/Product1.png' alt='Raclette1' style='width:100%'></a>
+    <h1>La raclette, simple, basique</h1>
 </div>
 <div class="commentaires">
     <h1><?php  include("../views/commentaires.php");?></h1>
