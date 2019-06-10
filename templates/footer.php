1c1d1781193c2cc7aa99e8c29898b3d9fbc4f176
Damsodo <damienuzzz@gmail.com>
2019-06-10 12:49:50 +0200

    xd

--- templates/footer.php
+++ templates/footer.php
@@ -91,19 +91,15 @@
 
         <!-- Facebook -->
         <a type="button" class="btn-floating btn-fb">
-          <i class="fab fa-facebook-f"></i>
+          <img src="../public/images/logoFB.png">
         </a>
         <!-- Twitter -->
         <a type="button" class="btn-floating btn-tw">
-          <i class="fab fa-twitter"></i>
+          <img src="../public/images/logoinsta.png">
         </a>
         <!-- Google +-->
         <a type="button" class="btn-floating btn-gplus">
-          <i class="fab fa-google-plus-g"></i>
-        </a>
-        <!-- Dribbble -->
-        <a type="button" class="btn-floating btn-dribbble">
-          <i class="fab fa-dribbble"></i>
+          <img src="../public/images/logoSNAP.png">
         </a>
 
       </div>
--- views/signInView.php
+++ views/signInView.php
@@ -38,7 +38,7 @@
       <br>
       Mot de Passe: <input type='password' name='password' required>
       <br>
-      <input type='checkbox' name='legalAgreement'>J'accepte les mentions légales ta mère la pute <br>
+      <input type='checkbox' name='legalAgreement'>J'accepte les mentions légales stp <br>
       <input type='submit' name='submit' value='Inscription'>
 
     </form>
