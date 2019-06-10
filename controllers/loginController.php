146326311e4228a95d3245f36c98520fc070d29e
Lem0n33 <thedeadteam1814@gmail.com>
2019-06-10 22:30:04 +0200

    debug login

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -44,13 +44,13 @@
 
         $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
         $active = $row['active'];
-        echo 'Test validé';
-        /*
+
+
         $count = mysqli_num_rows($result);
 
         // If result matched $myusername and $mypassword, table row must be 1 row
-
-        if($count == 1)
+        echo 'Test validé';
+        /*if($count == 1)
         {
             session_register("myusername");
             $_SESSION['login_user'] = $myusername;
