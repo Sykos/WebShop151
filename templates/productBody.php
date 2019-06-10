a2e0a4def74b4a34470d76a2cb3bb70c819e3e9f
Lem0n33 <thedeadteam1814@gmail.com>
2019-06-10 10:26:10 +0200

    Ajout lien produt to productDetail

--- templates/productBody.php
+++ templates/productBody.php
@@ -1,7 +1,7 @@
 <div class='cardProduct'>
-    <img src='../public/images/Product1.png' alt='Raclette1' style='width:100%'>
+    <a href="?page=productDetail"><img src='../public/images/Product1.png' alt='Raclette1' style='width:100%'></a>
     <h1>Raclette classique</h1>
     <p class='price'>$15.99</p>
     <p>Raclette classique au goût savoureux</p>
-    <p><a href="?page=productDetail"><button>Ajouter au panier</button></a></p>
+    <p><button>Ajouter au panier</button></p>
 </div>
