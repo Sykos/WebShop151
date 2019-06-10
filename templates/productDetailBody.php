bae7a2d9ea4b87eb0fc6f4269d2bd31dd47811b8
Damsodo <damienuzzz@gmail.com>
2019-06-10 20:27:24 +0200

    miaou

--- templates/productDetailBody.php
+++ templates/productDetailBody.php
@@ -5,6 +5,6 @@
     <h1>Ici se trouvera la description des produits</h1>
 </div>
 <div class="commentaires">
-    <h1><?php  include('commentaires.php'); echo"chat";?></h1>
+    <h1><?php  require('commentaires.php'); echo"minou";?></h1>
 
 </div>
