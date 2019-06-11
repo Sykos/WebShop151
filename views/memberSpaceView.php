bf5f3821633b36efb330df0c5df6c44d8ef29022
DONATANTONIO Nils <ndonatantonio@audemarspiguet.local>
2019-06-11 16:15:19 +0200

    Design memberspace

--- views/memberSpaceView.php
+++ views/memberSpaceView.php
@@ -20,22 +20,41 @@
         ?>
     </head>
     <body>
-        <div align="center">
-           <h2>Edition de mon profil</h2>
-           <div align="left">
-              <form method="POST" action="" enctype="multipart/form-data">
-                 <label>Pseudo :</label>
-                 <input type="text" name="newpseudo" placeholder="Pseudo" value="<?php echo $user['pseudo']; ?>" /><br /><br />
-                 <label>Mail :</label>
-                 <input type="text" name="newmail" placeholder="Mail" value="<?php echo $user['mail']; ?>" /><br /><br />
-                 <label>Mot de passe :</label>
-                 <input type="password" name="newmdp1" placeholder="Mot de passe"/><br /><br />
-                 <label>Confirmation - mot de passe :</label>
-                 <input type="password" name="newmdp2" placeholder="Confirmation du mot de passe" /><br /><br />
-                 <input type="submit" value="Mettre à jour mon profil !" />
-              </form>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <?php require_once './templates/header.php'; ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <?php require_once './templates/nav.php'; ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="wrapper">
+                        <div align="center">
+                           <h2>Edition de mon profil</h2>
+                           <div align="left">
+                              <form method="POST" action="" enctype="multipart/form-data">
+                                 <label>Pseudo :</label>
+                                 <input type="text" name="newpseudo" placeholder="Pseudo" value="<?php echo $user['pseudo']; ?>" class="form-control"/><br /><br />
+                                 <label>Mail :</label>
+                                 <input type="text" name="newmail" placeholder="Mail" value="<?php echo $user['mail']; ?>" class="form-control" /><br /><br />
+                                 <label>Mot de passe :</label>
+                                 <input type="password" name="newmdp1" placeholder="Mot de passe" class="form-control"/><br /><br />
+                                 <label>Confirmation - mot de passe :</label>
+                                 <input type="password" name="newmdp2" placeholder="Confirmation du mot de passe" class="form-control" /><br /><br />
+                                 <input type="submit" value="Mettre à jour mon profil !" class="btn btn-primary"/>
+                              </form>
+                           </div>
+                    </div>
+                </div>
+            </div>
 
-           </div>
+            </div>
         </div>
+
     </body>
 </html>
