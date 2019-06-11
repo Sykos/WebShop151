33af1c3d53407cfff479f2b1b8eae95f1c8064db
DONATANTONIO Nils <ndonatantonio@audemarspiguet.local>
2019-06-11 15:53:20 +0200

    Création memberspace

--- controllers/memberSpaceController.php
+++ controllers/memberSpaceController.php
@@ -14,36 +14,66 @@
             parent::render();
         }
 
+        public static function connexionToDB(){
+            $bdd = new PDO('mysql:host=127.0.0.1:49386;dbname=raclettev2', 'azure', '6#vWHD_$');
+            return $bdd;
+        }
+
+        public static function setUsername()
+        {
+            self::connexionToDB();
+            if(isset($_SESSION['id']))
+            {
+               $requser = $bdd->prepare("SELECT * FROM utilisateurs WHERE UtilisateursID = ?");
+               $requser->execute(array($_SESSION['id']));
+               $user = $requser->fetch();
+               if(isset($_POST['newpseudo']) AND !empty($_POST['newpseudo']) AND $_POST['newpseudo'] != $user['pseudo']) {
+                  $newpseudo = htmlspecialchars($_POST['newpseudo']);
+                  $insertpseudo = $bdd->prepare("UPDATE utilisateurs SET UtilisateursUser = ? WHERE UtilisateursID = ?");
+                  $insertpseudo->execute(array($newpseudo, $_SESSION['id']));
+                  header('Location: profil.php?id='.$_SESSION['id']);
+               }
+            }
+        }
+
+        public static function setEmail()
+        {
+            if(isset($_SESSION['id']))
+            {
+                if(isset($_POST['newmail']) AND !empty($_POST['newmail']) AND $_POST['newmail'] != $user['mail'])
+                {
+                   $newmail = htmlspecialchars($_POST['newmail']);
+                   $insertmail = $bdd->prepare("UPDATE utilisateurs SET UtilisateursEmail = ? WHERE UtilisateursID = ?");
+                   $insertmail->execute(array($newmail, $_SESSION['id']));
+                   header('Location: profil.php?id='.$_SESSION['id']);
+                }
+            }
+
+        }
+
+        public static function setPassword()
+        {
+            if(isset($_SESSION['id']))
+            {
+                if(isset($_POST['newmdp1']) AND !empty($_POST['newmdp1']) AND isset($_POST['newmdp2']) AND !empty($_POST['newmdp2']))
+                {
+                   $mdp1 = sha1($_POST['newmdp1']);
+                   $mdp2 = sha1($_POST['newmdp2']);
+                   if($mdp1 == $mdp2)
+                   {
+                      $insertmdp = $bdd->prepare("UPDATE utilisateurs SET UtilisateursMotDePasse = ? WHERE UtilisateursID = ?");
+                      $insertmdp->execute(array($mdp1, $_SESSION['id']));
+                      header('Location: profil.php?id='.$_SESSION['id']);
+                  }else
+                   {
+                      $msg = "Vos deux mdp ne correspondent pas !";
+                   }
+                }
+            }
+
+        }
+
+
+
 
     }
-    session_start();
-
-    //$bdd = new PDO('mysql:host=127.0.0.1;dbname=espace_membre', 'root', '');
-
-    if(isset($_SESSION['id'])) {
-     $requser = $bdd->prepare("SELECT * FROM membres WHERE id = ?");
-     $requser->execute(array($_SESSION['id']));
-     $user = $requser->fetch();
-     if(isset($_POST['newpseudo']) AND !empty($_POST['newpseudo']) AND $_POST['newpseudo'] != $user['pseudo']) {
-        $newpseudo = htmlspecialchars($_POST['newpseudo']);
-        $insertpseudo = $bdd->prepare("UPDATE membres SET pseudo = ? WHERE id = ?");
-        $insertpseudo->execute(array($newpseudo, $_SESSION['id']));
-        header('Location: profil.php?id='.$_SESSION['id']);
-     }
-     if(isset($_POST['newmail']) AND !empty($_POST['newmail']) AND $_POST['newmail'] != $user['mail']) {
-        $newmail = htmlspecialchars($_POST['newmail']);
-        $insertmail = $bdd->prepare("UPDATE membres SET mail = ? WHERE id = ?");
-        $insertmail->execute(array($newmail, $_SESSION['id']));
-        header('Location: profil.php?id='.$_SESSION['id']);
-     }
-     if(isset($_POST['newmdp1']) AND !empty($_POST['newmdp1']) AND isset($_POST['newmdp2']) AND !empty($_POST['newmdp2'])) {
-        $mdp1 = sha1($_POST['newmdp1']);
-        $mdp2 = sha1($_POST['newmdp2']);
-        if($mdp1 == $mdp2) {
-           $insertmdp = $bdd->prepare("UPDATE membres SET motdepasse = ? WHERE id = ?");
-           $insertmdp->execute(array($mdp1, $_SESSION['id']));
-           header('Location: profil.php?id='.$_SESSION['id']);
-        } else {
-           $msg = "Vos deux mdp ne correspondent pas !";
-        }
-     }
