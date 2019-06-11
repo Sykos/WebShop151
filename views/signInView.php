ced76ae2d11350358f771726c99e275eb36feb2b
DONATANTONIO Nils <ndonatantonio@audemarspiguet.local>
2019-06-11 12:07:56 +0200

    Amélioration signIn

--- views/signInView.php
+++ views/signInView.php
@@ -22,33 +22,65 @@ error_reporting(E_ALL);
     ?>
   </head>
   <body>
-    <?php require_once './templates/header.php'; ?>
-    <?php require_once './templates/nav.php'; ?>
-    <form class='' action='?page=signIn' method='post'>
-      Titre:
-      <select name='titre'>
-        <option>M.</option>
-        <option>Mme</option>
-      </select>
-      <br>
-      
-      Nom: <input type='text' name='nom' required>
-      <br>
-      Prénom: <input type='text' name='prenom' required>
-      <br>
-      Date de naissance: <input type='text' name='dateOfBirth' required>
-      <br>
-      E-mail: <input type='text' name='email' required>
-      <br>
-      Nom d'utilisateur: <input type='text' name='username' required>
-      <br>
-      Mot de Passe: <input type='password' name='password' required>
-      <br>
-      <input type='checkbox' name='legalAgreement'>J'accepte les mentions légales stp <br>
-      <input type='submit' name='submit' value='Inscription'>
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col-md-12">
+                  <?php require_once './templates/header.php'; ?>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-md-12">
+                  <?php require_once './templates/nav.php'; ?>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-md-12">
+                  <div class="wrapper">
+                      <h2>Inscription</h2>
+                      <p>Insérez vos informations pour créer votre compte</p>
+                      <form action="?page=signIn" method="post">
+                          <div>
+                              <label>Nom : </label>
+                              <input type="text" name="nom" class="form-control">
+                          </div>
+                          <div>
+                              <label>Prénom : </label>
+                              <input type="text" name="prenom" class="form-control">
+                          </div>
+                          <div>
+                              <label>Date de naissance : </label>
+                              <input type="text" name="dateDeNaissance" class="form-control">
+                          </div>
+                          <div>
+                              <label>E-mail : </label>
+                              <input type="text" name="email" class="form-control">
+                          </div>
+                          <div>
+                              <label>Nom d'utilisateur : </label>
+                              <input type="text" name="username" class="form-control">
+                          </div>
+                          <div>
+                              <label>Mot de passe : </label>
+                              <input type="password" name="password" class="form-control">
+                          </div>
+                          <div>
+                              <label>Confirmer mot de passe : </label>
+                              <input type="password" name="confirmPassword" class="form-control">
+                          </div>
+                          <div>
+                              <input type="submit" name="submit" value="S'enregistrer" class="btn btn-default">
+                          </div>
+                      </form>
+                  </div>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-md-12">
+                  <?php require_once './templates/footer.php'; ?>
+              </div>
+          </div>
+      </div>
 
-    </form>
-    <?php require_once './templates/footer.php'; ?>
 
 
   </body>
